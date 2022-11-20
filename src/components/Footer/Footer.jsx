d04ab6c66293c04e15e8d6ceeac30d12d7f2ce58
nicolasCimino <nico.cimino.90@gmail.com>
2022-11-20 19:00:04 -0300

    test

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,15 +7,15 @@ function  Footer() {
         <ul>
             <li>             
                 <img src="https://res.cloudinary.com/dtvv5h3mm/image/upload/v1668976259/cursoReactJS/iconWhatsapp_fwz3s8.svg" alt="iconWhatsapp"/>
-                <a href="https://web.whatsapp.com/" target="_blank" >11-xxxx-xxxx</a>
+                <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer" >11-xxxx-xxxx</a>
             </li>
             <li>
                 <img src="https://res.cloudinary.com/dtvv5h3mm/image/upload/v1668976259/cursoReactJS/iconFacebook_oyicn9.svg" alt="iconFacebook"/>
-                <a href="https://www.facebook.com/" target="_blank">Amor a Primera Pinta</a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">Amor a Primera Pinta</a>
             </li>
             <li>
                 <img src="https://res.cloudinary.com/dtvv5h3mm/image/upload/v1668976259/cursoReactJS/iconInstagram_klylnp.svg" alt="iconInstagram"/>
-                <a href="https://www.instagram.com/" target="_blank">Amor.PrimeraPintaOK</a>
+                <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">Amor.PrimeraPintaOK</a>
             </li>
         </ul>
     </footer>
