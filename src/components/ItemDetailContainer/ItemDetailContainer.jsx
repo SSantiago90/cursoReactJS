574d146903a7dcdafdec6a6b80dc9ce0abfbcf58
nicolasCimino <nico.cimino.90@gmail.com>
2022-12-09 12:13:21 -0300

    test 2

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { getItemByID } from "../../services/mockService";
 import { useParams } from "react-router-dom";
-import ItemDetail from "./ItemDetail";
+import ItemDetail from "./ItemDetail/ItemDetail";
 
 function  ItemDetailContainer() {
     const [product, setProduct] = useState([]);
     const itemID = useParams().IDproducto;
+
     useEffect(() => {
         getItemByID(itemID)
           .then((respuesta) => {
             setProduct(respuesta);
           })
           .catch((error) => alert("Item no encontrado"));
-      }, []); 
+      }, [itemID]); 
     return (
     <>
         <ItemDetail 
