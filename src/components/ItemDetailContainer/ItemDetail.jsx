90b134d044b725a1c50f5d9ccb94f580722ee6bf
nicolasCimino <nico.cimino.90@gmail.com>
2022-12-09 01:13:49 -0300

    srcSet warning

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,8 +1,9 @@
 function  ItemDetail(props) {
+  let altText = `imagen  + ${props.titulo}`;
     return (
     <>
         <h1>{props.titulo}</h1>
-        <img src={props.img} alt=""/>
+        <img src={props.img}  alt ={altText} srcSet="" />
         <p>{props.descripcion}</p>
         <p>$ {props.precio}</p>
     </>
--- src/components/ItemListContainer/Item/Item.jsx
+++ src/components/ItemListContainer/Item/Item.jsx
@@ -2,12 +2,13 @@ import "./item.css"
 import { Link } from "react-router-dom";
 
 function  Item(props) {
+    let altText = `imagen  + ${props.titulo}`;
     return (
     <>
         <Link to={`/item/${props.id}`}>
             <div className="item">
                 <h5>{props.titulo}</h5>
-                <img className="imgItem" src={props.img} alt="" srcset="" />
+                <img className="imgItem" src={props.img} alt ={altText} srcSet="" />
                 <p>$ {props.precio}</p>
             </div>
         </Link>
