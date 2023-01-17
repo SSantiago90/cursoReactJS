8a17f7ea0c0dc68447d5618c44605765b517f492
nicolasCimino <nico.cimino.90@gmail.com>
2023-01-16 23:16:19 -0300

    buttoncount con condicional

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -4,15 +4,17 @@ import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail/ItemDetail";
 import "./itemDetailContainer.css"
 import ButtonCount from "../Buttons/ButtonCount/ButtonCount";
+import MyButton from "../Buttons/MyButton/MyButton";
 import { cartContext } from "../../storage/cartContext";
 import Loader from "../Loader/Loader";
+import { Link } from "react-router-dom";
 
 function  ItemDetailContainer() {
     const [product, setProduct] = useState([]);
     const [Loading, setLoading] = useState(true);
+    const [isEmpty, setIsEmpty] = useState(true);
     const itemID = useParams().IDproducto;
     const {addToCart} = useContext(cartContext);
-
 
 useEffect(() => {
       getItemByID(itemID)
@@ -25,6 +27,7 @@ useEffect(() => {
 
     function handleAddToCart(valor){
       addToCart(product,valor);
+      setIsEmpty(false);
     }
     
     return (
@@ -33,7 +36,16 @@ useEffect(() => {
         Loading ? <Loader/> :
         <div className="itemDetail">
           <ItemDetail producto ={product}/>
-          <ButtonCount stock = {product.stock} finishCount={handleAddToCart}/>   
+          {isEmpty ? 
+            <ButtonCount stock = {product.stock} finishCount={handleAddToCart}/> 
+            : 
+           <Link to={"/cart"}>
+            <MyButton
+              text = "Ir al carrito..."
+              color="outline-success"
+              />
+           </Link>
+          }   
         </div>
       }
     </>
